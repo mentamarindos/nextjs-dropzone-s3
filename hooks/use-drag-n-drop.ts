97df5a370ac04d510efd443c368651c114f58a40
mentamarindos <mentamarino@gmail.com>
2023-04-19 04:49:54 -0600

    add eslint, standard and typescript

--- hooks/use-drag-n-drop.ts
+++ hooks/use-drag-n-drop.ts
@@ -1,35 +1,35 @@
 import { useDropzone } from 'react-dropzone'
 import { useState, useCallback } from 'react'
 
-export default function useDragAndDrop() {
-    const [myFiles, setMyFiles] = useState([])
+export default function useDragAndDrop () {
+  const [myFiles, setMyFiles] = useState([])
 
-    const onDrop = useCallback(acceptedFiles => {
+  const onDrop = useCallback(acceptedFiles => {
     // setMyFiles([...myFiles, ...acceptedFiles])
     setMyFiles(acceptedFiles.map(file => {
-        // if(file.)
+      // if(file.)
 
-        return Object.assign(file, {
-            preview: URL.createObjectURL(file)
-        })
-        })
+      return Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })
+    })
     )
-    }, [myFiles])
+  }, [myFiles])
 
-    const { getRootProps, getInputProps } = useDropzone({ onDrop })
+  const { getRootProps, getInputProps } = useDropzone({ onDrop })
 
-    function removeFile(file) {
-return () => {
-    const newFiles = [...myFiles]
-    newFiles.splice(newFiles.indexOf(file), 1)
-    setMyFiles(file => newFiles)
-}
+  function removeFile (file) {
+    return () => {
+      const newFiles = [...myFiles]
+      newFiles.splice(newFiles.indexOf(file), 1)
+      setMyFiles(file => newFiles)
     }
+  }
 
-    return {
-        myFiles,
-removeFile,
-        getRootProps,
-        getInputProps
-    }
+  return {
+    myFiles,
+    removeFile,
+    getRootProps,
+    getInputProps
+  }
 }
